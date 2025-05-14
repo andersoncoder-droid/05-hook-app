d5e36d39e1b481b804d6b69dd1d63a03d387813b
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-13 20:23:02 -0500

    Update main.jsx to render SimpleForm component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { CounterApp } from './01-useState/CounterApp';
+import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+// import { CounterApp } from './01-useState/CounterApp';
 // import { HooksAPP } from './HooksAPP';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterApp />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <SimpleForm />
+  // </React.StrictMode>
 );
