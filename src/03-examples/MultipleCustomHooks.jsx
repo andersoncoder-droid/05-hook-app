3f2cb90ded591b583b214838ca253778660001ab
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-14 14:56:54 -0500

    Add useFetch hook

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,14 @@
+import { useFetch } from '../hooks';
+
+export const MultipleCustomHooks = () => {
+  const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/1');
+
+  return (
+    <>
+      <h1>Informacion de Pokémon</h1>
+      <hr />
+      {isLoading && <p>Cargando...</p>}
+      <h2>{data?.name}</h2>
+    </>
+  );
+};
